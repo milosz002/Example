191996778f4acb604146c899e54d7c3d7058ec69
milosz002 <milosz0192@gmail.com>
2022-11-14 11:25:07 +0100

    Test class

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-
-def Test(): 
-    print("TEST")
+class Test:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}"
         
-Test()
+person = Test("TestName", 91)
+print(person)
